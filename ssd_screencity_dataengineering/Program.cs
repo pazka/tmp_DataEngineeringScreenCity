521e76993dc72ec213e55fbd75d65b7bd1a9c667
unknown <alexandre.weisser@gmail.com>
2024-04-09 23:50:58 +0200

    created sorted csv file

--- ssd_screencity_dataengineering/Program.cs
+++ ssd_screencity_dataengineering/Program.cs
@@ -1,28 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Numerics;
 using MySql.Data.MySqlClient;
 
 namespace ssd_screencity_dataengineering
 {
     class Program
     {
+        static int ProgressionSteps = 500000;
+        static float MaxLines = 34636765;
         static void Main(string[] args)
         {
-            MySqlConnection myConnection;
-            string myConnectionString;
-            //set the correct values for your server, user, password and database name
-            myConnectionString = "server=localhost;uid=root;pwd=root;database=data";
+            StoreCsv();
+        }
 
-            try
+        static void PrintProgression(int i)
+        {
+            if (i % Program.ProgressionSteps == 0)
             {
-                myConnection = new MySqlConnection(myConnectionString);
-                //open a connection
-                myConnection.Open();
-                AssignCsvToTable(myConnection);
-            }
-            catch (MySqlException ex)
-            {
-                Console.WriteLine(ex.Message);
+                Console.WriteLine(i / Program.MaxLines * 100.0 + "%");
             }
         }
 
@@ -42,67 +39,59 @@ namespace ssd_screencity_dataengineering
             return count;
         }
 
-        public static void AssignCsvToTable(MySqlConnection myConnection)
+
+        public static void StoreCsv()
         {
-            string path = @"C:\Users\Alexa\Documents\Projects\Alessia\Seine-Saint-Denis_ScreenCityDataEngineering\ssd_screencity_dataengineering\data\GeolocalisationEtablissement_Sirene_pour_etudes_statistiques_utf8.csv";
-            float maxLines = 34636765 ;
+            string path = @"C:\Users\Pazka\Documents\Code\Seine-Saint-Denis_ScreenCityDataEngineering\ssd_screencity_dataengineering\data\GeolocalisationEtablissement_Sirene_pour_etudes_statistiques_utf8.csv";
+
             CountLinesInFile(path);
+            string[][] dataLines = new string[34636765][]; // 34M lines
+            SortedDictionary<string, int> linesIndexes = new SortedDictionary<string, int>();
 
-            StreamReader file = new StreamReader(path);
-            string line;
-            float i = 0;
-            line = file.ReadLine(); // skip the first line
-            while (line != null)
+            using (StreamReader file = new StreamReader(path))
             {
-                line = file.ReadLine();
-                if(i < 25977573) // skip the first 17M lines (already in the database)
+                int i = 0;
+                file.ReadLine(); // skip the first line
+
+                string line = file.ReadLine();
+                Console.WriteLine("Reading file...");
+                do
                 {
+                    PrintProgression(i);
+
+                    string[] lineArray = line.Split(";");
+                    string lineIdx = lineArray[0];
+                    dataLines[i] = lineArray;
+                    linesIndexes.Add(lineIdx, i);
+
+                    line = file.ReadLine();
                     i++;
-                    continue;
-                }
+                } while (line != null);
 
-                if (i % 1000 == 0)
-                {
-                    Console.WriteLine((i / maxLines ) * 100 + "%");
-                }
-
-                line = file.ReadLine();
-                string[] lineArray = line.Split(";");
-                AddNewLineInTable(lineArray, myConnection);
-                i++;
+                Console.WriteLine("Went up to " + i + " lines in the file.");
             }
 
-            Console.WriteLine("Went up to " + i + " lines in the file.");
-            file.Close();
+            WriteSortedCSV(dataLines, linesIndexes);
         }
 
-        public static void AddNewLineInTable(string[] line, MySqlConnection myConnection)
+        public static void WriteSortedCSV(string[][] dataLines, SortedDictionary<string, int> linesIndexes)
         {
-            // create a MySQL command and set the SQL statement with parameters
-            MySqlCommand myCommand = new MySqlCommand();
-            myCommand.Connection = myConnection;
-            myCommand.CommandText = @"INSERT INTO geoloc (siret, x, y, qualite_xy, epsg, plg_qp, plg_iris, plg_zus, plg_qva, plg_code_commune, distance_precision, qualite_qp, qualite_iris, qualite_zus, qualite_qva, y_latitude, x_longitude) VALUES (@siret, @x, @y, @qualite_xy, @epsg, @plg_qp, @plg_iris, @plg_zus, @plg_qva, @plg_code_commune, @distance_precision, @qualite_qp, @qualite_iris, @qualite_zus, @qualite_qva, @y_latitude, @x_longitude);";
-            myCommand.Parameters.AddWithValue("@siret", line[0]);
-            myCommand.Parameters.AddWithValue("@x", line[1]);
-            myCommand.Parameters.AddWithValue("@y", line[2]);
-            myCommand.Parameters.AddWithValue("@qualite_xy", line[3]);
-            myCommand.Parameters.AddWithValue("@epsg", line[4]);
-            myCommand.Parameters.AddWithValue("@plg_qp", line[5]);
-            myCommand.Parameters.AddWithValue("@plg_iris", line[6]);
-            myCommand.Parameters.AddWithValue("@plg_zus", line[7]);
-            myCommand.Parameters.AddWithValue("@plg_qva", line[8]);
-            myCommand.Parameters.AddWithValue("@plg_code_commune", line[9]);
-            myCommand.Parameters.AddWithValue("@distance_precision", line[10]);
-            myCommand.Parameters.AddWithValue("@qualite_qp", line[11]);
-            myCommand.Parameters.AddWithValue("@qualite_iris", line[12]);
-            myCommand.Parameters.AddWithValue("@qualite_zus", line[13]);
-            myCommand.Parameters.AddWithValue("@qualite_qva", line[14]);
-            myCommand.Parameters.AddWithValue("@y_latitude", line[15]);
-            myCommand.Parameters.AddWithValue("@x_longitude", line[16]);
+            string path = @"C:\Users\Pazka\Documents\Code\Seine-Saint-Denis_ScreenCityDataEngineering\ssd_screencity_dataengineering\data\GeolocalisationEtablissement_Sirene_pour_etudes_statistiques_utf8_sorted.csv";
+            int i = 0;
+            using (StreamWriter file = new StreamWriter(path))
+            {
+                Console.WriteLine("Writing to file...");
 
+                foreach (var lineIdx in linesIndexes)
+                {
+                    PrintProgression(i);
+                    string[] line = dataLines[lineIdx.Value];
+                    string lineStr = string.Join(";", line);
+                    file.WriteLine(lineStr);
+                }
+            }
 
-            // execute the command
-            myCommand.ExecuteNonQuery();
+            Console.WriteLine("Wrote " + i + " lines in the file.");
         }
     }
 }
