c0ce1663d810b69bfdec3c60b45e94483e102664
Alexandre WEISSER <alexandre.weisser@gmail.com>
2024-04-13 01:30:26 +0200

    cleaned etablissement file

--- etablissement_cleaning/clean_file.py
+++ etablissement_cleaning/clean_file.py
@@ -8,37 +8,50 @@
 # - coordonneeLambertAbscisseEtablissement
 # - coordonneeLambertOrdonneeEtablissement
 
+splited_lines = []
 newLines = []
 i = 0
-with open('./etablissements_geoloc.csv','r',encoding="utf8") as f:
+with open('C:/Users/Alexa/Documents/Projects/Alessia/Seine-Saint-Denis_ScreenCityDataEngineering/etablissement_cleaning/etablissements_geoloc.csv', 'r', encoding="utf8") as f:
     lines = f.readlines()
     header = lines[0].split(',')
     example = lines[5].split(',')
-    j=0
+    j = 0
     for headerName in header:
-        print(j,":",headerName,example[j])
-        j+=1
-    
+        print(j, ":", headerName, example[j])
+        j += 1
+
     for line in lines[1:]:
-        i+=1
+        
         data = line.split(',')
+        if len(data) > 83:
+            continue
+        splited_lines.append(data)
+
+    splited_lines.sort(key=lambda x: x[4])
+
+    for data in splited_lines: 
+        i += 1
+
+
+        geoloc = data[82].replace('\n', '')
+        X = geoloc.split(';')[15]
+        Y = geoloc.split(';')[16]
         newLines.append({
             "siren": data[0],
             "dateCreationEtablissement": data[4],
             "denominationUniteLegale": data[15],
-            "coordonneeLambertAbscisseEtablissement": data[58],
-            "coordonneeLambertOrdonneeEtablissement": data[59]
+            "X": X,
+            "Y": Y,
+            "IS_ONE_PERSON" : data[14] == "1000" # categorie juridique unité legal is Entrepreneur indiciuel  == 1000
         })
         if i % 1000 == 0:
             print(f'{i/len(lines) * 100} %')
 
-with open('./etablissements_geoloc_cleaned.csv', 'w',encoding='utf8') as f:
+with open('C:/Users/Alexa/Documents/Projects/Alessia/Seine-Saint-Denis_ScreenCityDataEngineering/etablissement_cleaning/etablissements_geoloc_cleaned.csv', 'w', encoding='utf8') as f:
 
-    f.write('siren,dateCreationEtablissement,denominationUniteLegale,coordonneeLambertAbscisseEtablissement,coordonneeLambertOrdonneeEtablissement\n')
+    f.write('siren,dateCreationEtablissement,denominationUniteLegale,isOnePerson,X,Y\n')
 
     for line in newLines:
-        f.write(f"{line['siren']},{line['dateCreationEtablissement']},{line['denominationUniteLegale']},{line['coordonneeLambertAbscisseEtablissement']},{line['coordonneeLambertOrdonneeEtablissement']}\n")
+        f.write(f"{line['siren']},{line['dateCreationEtablissement']},{line['denominationUniteLegale']},{line['IS_ONE_PERSON']},{line['X']},{line['Y']}\n")
 
 print('Done')
-
-
